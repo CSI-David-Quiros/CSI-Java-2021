f659df6697debce180afea0c8a11f9068d284b0c
CSI-David-Quiros <24-068@sanginacio.pr>
2021-09-14 08:59:09 -0400

    Animal Farm

--- MainProject/src/csi/quiros/pkg/AnimalFarm.java
+++ MainProject/src/csi/quiros/pkg/AnimalFarm.java
@@ -4,15 +4,19 @@ package csi.quiros.pkg;
 	 static Animal[] animals ;
 	 public static void main(String[] args){
 			animals = new Animal[5];
-			animals[0] = new Animal();
+			animals[0] = new Animal("Horse",30,"Equus caballus", new Taxonomy("Eukaryote","Animalia","Chordate","Mammalia","Perissodactyla","Equidae","Equus Linnaeus","Equus caballus"));
+			animals[1] = new Animal("Goldfish",3,"Carassius auratus", new Taxonomy("Eukaryote","Animalia","Chordate","Actinopterygii","Cypriniformes","Cyprinidae","Carassius","Carassius auratus"));
+			animals[2] = new Animal("Aye-aye",22,"Daubentonia madagascariensis", new Taxonomy("Eukaryote","Animalia","Chordata","Mammalia","Primates","Strepsirrhini","Daubentonia","D.madagascariensis"));
+			animals[3] = new Animal("Capybara",9,"Hydrochoerus hydrochaeris", new Taxonomy("Eukaryote","Animalia","Chordate","Mammalia","Rodentia","Caviidae","	Hydrochoerus","Hydrochoerus hydrochaeris"));
+			animals[4] = null;
 			
-			
-			
-			
-			
-			
-			
-			
+				for(int i = 0; i < animals.length; i++) {
+					System.out.println(animals[i]);
+					
+					
+					
+					
+				}		
 			
 			
 	 }
