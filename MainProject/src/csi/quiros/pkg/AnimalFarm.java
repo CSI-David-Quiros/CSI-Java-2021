e6a78782ad7ff5afea364ac7996ec2d78f3fa23a
CSI-David-Quiros <24-068@sanginacio.pr>
2021-09-13 22:08:42 -0400

    no message

---
+++ MainProject/src/csi/quiros/pkg/AnimalFarm.java
@@ -0,0 +1,20 @@
+package csi.quiros.pkg;
+
+	public class AnimalFarm {
+	 static Animal[] animals ;
+	 public static void main(String[] args){
+			animals = new Animal[5];
+			animals[0] = new Animal();
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+	 }
+}
+
