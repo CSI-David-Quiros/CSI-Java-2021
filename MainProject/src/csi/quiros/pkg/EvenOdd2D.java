825fd2b88871f699e15ba13f506e344fac04c392
CSI-David-Quiros <24-068@sanginacio.pr>
2021-09-24 14:37:49 -0400

    Arreglo dos dimensiones

---
+++ MainProject/src/csi/quiros/pkg/EvenOdd2D.java
@@ -0,0 +1,22 @@
+package csi.quiros.pkg;
+
+public class EvenOdd2D {
+
+
+		public static void main(String[] args) {
+			int[][] arr = new int[10][10];
+
+			for(int i = 0; i < arr.length; i++) {
+			    for(int j = 0; j < arr[i].length; j++) {
+			    }
+			    System.out.println(arr[i]);
+			    evenOddPrint(arr);
+			}
+		}
+			public static void evenOddPrint(int[][] arr) {
+		
+	}
+
+
+NoEntinedo;
+}
